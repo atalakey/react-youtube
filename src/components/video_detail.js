64ba7a199587ad672a36ce61ff34bd16fb257d7c
Abdullah Talakey <abdullah.talakey@gmail.com>
2018-09-29 16:26:32 +0300

    Add styling

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const VideoDetail = (props) => {
   return (
-    <div>
-      <div>
+    <div className="col-md-8 video-detail">
+      <div className="embed-responsive embed-responsive-16by9">
         Video Player
       </div>
-      <div>
+      <div className="details">
         <div>{props.video.title}</div>
         <div>{props.video.description}</div>
       </div>
--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const VideoListItem = (props) => {
   return (
-    <li onClick={() => props.onVideoSelect(props.video)}>
-      <div>
-        <div>
+    <li onClick={() => props.onVideoSelect(props.video)} className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
           <img src="/" alt="Some Pic" />
         </div>
-        <div>
-          <div>{props.video.title}</div>
+        <div className="media-body">
+          <div className="media-heading">{props.video.title}</div>
         </div>
       </div>
     </li>
